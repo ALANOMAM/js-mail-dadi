5ed576a52cfad4bb34dec10f08dbd34f68d85d79
alan_omam <omamalan@hotmail.it>
2024-02-01 17:46:11 +0100

    PARTE LOGICA FATTA( cambiato metodo dopo avere parlato col tutor. )

--- MAIL/index.js
+++ MAIL/index.js
@@ -5,26 +5,27 @@ controlla che sia nella lista di chi può accedere (qui quindi dovremmo creare d
 stampa un messaggio appropriato sull’esito del controllo.
 */
 
-const userMail = "omam@alam.com"
-const mails = ["omam@alan.com","marco@mengoni.com","nloga@andy.com","violani@pietro.com","spanu@gabriel.com"]
-let index = mails.indexOf(userMail)
+let userMail = "omam@alam.com"
+let mails = ["omam@alan.com","marco@mengoni.com","nloga@andy.com","violani@pietro.com","spanu@gabriel.com"]
+
 
 const result = document.querySelector("#output")
  
 
 for(let i=0; i<mails.length; i++){
      
- if( index != -1){
+ if(userMail === mails[i] ){
     result.innerHTML="puoi accedere"
     console.log("puoi accedere") 
-    //per rompere il ciclo e non fare ripettere la stessa frase troppe volte
-    mails.length = 0;
-
+    //per uscire al ciclo
+    mails.length=0;
+    
  } else{
     result.innerHTML="non puoi accedere"
     console.log(" non puoi accedere") 
-    //per rompere il ciclo e non fare ripettere la stessa frase troppe volte 
-    mails.length = 0;
+    //per uscire al ciclo
+    mails.length=0;
  } 
+
  
 }
